7603c243418f78f75a3b2bfcba05970f031fbec3
wiltis09 <110127597+wiltis09@users.noreply.github.com>
2025-09-24 22:18:58 +0200

    test 3

--- dice_test.py
+++ dice_test.py
@@ -24,8 +24,33 @@ toss_frame_1_path = r"/workspaces/skole_dice_test/toss_frames/toss_frame_1_ASCII
 start_text_ASCII = r"/workspaces/skole_dice_test/start_text_ASCII.txt"
 reroll_text_ASCII = r"/workspaces/skole_dice_test/reroll_text_ASCII.txt"
 
+git_dice_num_1_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_1_ASCII.txt"
+git_dice_num_2_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_2_ASCII.txt"
+git_dice_num_3_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_3_ASCII.txt"
+git_dice_num_4_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_4_ASCII.txt"
+git_dice_num_5_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_5_ASCII.txt"
+git_dice_num_6_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/dice_side_num_6_ASCII.txt"
+
+git_toss_frame_6_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_6_ASCII.txt"
+git_toss_frame_5_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_5_ASCII.txt"
+git_toss_frame_4_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_4_ASCII.txt"
+git_toss_frame_3_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_3_ASCII.txt"
+git_toss_frame_2_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_2_ASCII.txt"
+git_toss_frame_1_path = r"/Users/wilton/Documents/GitHub/skole_dice_test/toss_frames/toss_frame_1_ASCII.txt"
+
+git_start_text_ASCII = r"/Users/wilton/Documents/GitHub/skole_dice_test/start_text_ASCII.txt"
+git_reroll_text_ASCII = r"/Users/wilton/Documents/GitHub/skole_dice_test/reroll_text_ASCII.txt"
 curnet_num = int(1)
 
+
+git_dice_num_frames = [
+    git_dice_num_1_path,
+    git_dice_num_2_path,
+    git_dice_num_3_path,
+    git_dice_num_4_path,
+    git_dice_num_5_path,
+    git_dice_num_6_path,
+]
 dice_num_frames = [
     dice_num_1_path,
     dice_num_2_path,
@@ -33,6 +58,15 @@ dice_num_frames = [
     dice_num_4_path,
     dice_num_5_path,
     dice_num_6_path,
+]
+
+git_toss_anim_frames = [
+    git_toss_frame_1_path,
+    git_toss_frame_2_path,
+    git_toss_frame_3_path,
+    git_toss_frame_4_path,
+    git_toss_frame_5_path,
+    git_toss_frame_6_path
 ]
 
 toss_anim_frames = [
@@ -52,21 +86,40 @@ def roll_num():
         print(f"Dice roll: {dice_roll}")
         return dice_roll
 def roll(roll_num, Speed,):
+    os.system("cls" if os.name == "nt" else "clear")  # clear screen
+    try:
         for frame in toss_anim_frames:
             os.system("cls" if os.name == "nt" else "clear")  # clear screen
             with open(frame, "r", encoding="utf-8") as f:
+                os.system("cls" if os.name == "nt" else "clear")  # clear screen
                 print(f.read())
             time.sleep(Speed)  # delay between frames
-
+            
         if frame == toss_anim_frames[5]:
             os.system("cls" if os.name == "nt" else "clear")  # clear screen
             with open(dice_num_frames[roll_num], "r", encoding="utf-8") as f:
-                print(f.read())
+                print(f.read())         
             time.sleep(2)  # delay between frames
             roll_number = str(roll_num + 1)
             print("you rolled a ", roll_number)
             print(input("Press Enter to roll again..."))
             return
+    except:
+            for frame in git_toss_anim_frames:
+                os.system("cls" if os.name == "nt" else "clear")  # clear screen
+                with open(frame, "r", encoding="utf-8") as f:
+                    print(f.read())
+                time.sleep(Speed)  # delay between frames
+            if frame == git_toss_anim_frames[5]:
+                os.system("cls" if os.name == "nt" else "clear")  # clear screen
+                with open(git_dice_num_frames[roll_num], "r", encoding="utf-8") as f:
+                    os.system("cls" if os.name == "nt" else "clear")  # clear screen
+                    print(f.read())
+                time.sleep(2)  # delay between frames
+                roll_number = str(roll_num + 1)
+                print("you rolled a ", roll_number)
+                print(input("Press Enter to roll again..."))
+                return
 
 while True:
     if start_up == False:
@@ -74,13 +127,16 @@ while True:
     elif start_up == True:
         start_up = False
         os.system("cls" if os.name == "nt" else "clear")  # clear screen
-        with open(start_text_ASCII, "r", encoding="utf-8") as f:
-            print(f.read())
-            print(input())
-            time.sleep(1)  # delay between frames
+        try:
+            with open(start_text_ASCII, "r", encoding="utf-8") as f:
+                print(f.read())
+                print(input())
+                time.sleep(1)  # delay between frames
+        except:
+            with open(git_start_text_ASCII, "r", encoding="utf-8") as f:
+                print(f.read())
+                print(input())
+                time.sleep(1)  # delay between frames
+        os.system("cls" if os.name == "nt" else "clear")  # clear screen
         roll(roll_num(), 0.3)
-        start_up = False
-        
-
-
-
+        start_up = bool(False)
